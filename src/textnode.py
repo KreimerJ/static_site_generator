73bfefeffbe9594579889ce28d19c20daea9701d
kreimer <kranterprog@gmail.com>
2026-09-11 13:21:03 -0500

    feat(textnode): implemented TextNode and TextType enum

---
+++ src/textnode.py
@@ -0,0 +1,61 @@
+from enum import Enum
+from typing import override
+
+from htmlnode import HTMLNode
+from leafnode import LeafNode
+from src import leafnode
+
+
+class TextType(Enum):
+    PLAIN = "plain text"
+    BOLD = "bold text"
+    ITALIC = "italic text"
+    CODE = "code text"
+    LINK = "link text"
+    IMAGE_LINK = "image link text"
+
+
+class TextNode:
+    def __init__(self, text: str, text_type: TextType, url: str | None = None):
+        self.text: str = text
+        self.text_type: TextType = text_type
+        self.url: str | None = url
+
+    @override
+    def __eq__(self, other: object, /) -> bool:
+        if not isinstance(other, TextNode):
+            return False
+
+        return (
+            self.text == other.text
+            and self.text_type == other.text_type
+            and self.url == other.url
+        )
+
+    def textnode_to_htmlnode(self) -> LeafNode:
+        match self.text_type:
+            case TextType.PLAIN:
+                return LeafNode(None, self.text)
+
+            case TextType.BOLD:
+                return LeafNode("b", self.text)
+
+            case TextType.ITALIC:
+                return LeafNode("i", self.text)
+
+            case TextType.CODE:
+                return LeafNode("code", self.text)
+
+            case TextType.LINK:
+                if not self.url:
+                    raise ValueError("url is required for link text")
+                return LeafNode("a", self.text, {"href": self.url})
+
+            case TextType.IMAGE_LINK:
+                if not self.url:
+                    raise ValueError("url is required for image link text")
+                return LeafNode("img", self.text, {"src": self.url})
+
+    @override
+    def __repr__(self) -> str:
+        return f"TextNode({self.text}, {self.text_type}, {self.url})"
