eba702090d8d0ba59a49d5194fe4b64b95df41d4
kreimer <kranterprog@gmail.com>
2026-09-23 21:15:18 -0500

    chore(main.py, gitignore) added public that is generated content to gitignore, also removed print statements from main.py

--- src/main.py
+++ src/main.py
@@ -2,45 +2,34 @@ import os
 import shutil
 
 
-def test():
-    print(os.getcwd())
-    public_dir = os.path.join(os.getcwd(), "public")
-    if os.path.exists(public_dir):
-        os.chdir(public_dir)
-        print(f"dir = {os.getcwd()}")
-
-
-def clean_previous_public_and_copy():
+def clean_public_and_copy_static_files():
     public_dir = os.path.join(os.getcwd(), "public")
     static_dir = os.path.join(os.getcwd(), "static")
     if os.path.exists(public_dir):
         shutil.rmtree(public_dir, ignore_errors=True)
-        print(f"removed {public_dir}")
         os.mkdir(public_dir)
-        print(f"created{public_dir}")
+    else:
+        os.mkdir(public_dir)
 
     copy_static_files(static_dir, public_dir)
 
 
 def copy_static_files(source_path, destination_path):
     if not os.path.exists(source_path):
-        return
+        raise FileNotFoundError(f"source path {source_path} does not exist")
 
     for item in os.listdir(source_path):
-        if os.path.isfile(item):
-            copy_static_files(os.path.join(source_path, item), destination_path)
-            print(
-                f"copied {item}, path = {os.path.join(source_path, item)}, destination = {destination_path}"
-            )
+        new_item_path = os.path.join(source_path, item)
+        new_destination_path = os.path.join(destination_path, item)
+        if os.path.isdir(new_item_path):
+            os.mkdir(new_destination_path)
+            copy_static_files(new_item_path, new_destination_path)
         else:
-            shutil.copy(os.path.join(source_path, item), destination_path)
-            print(
-                f"copied {item}, path = {os.path.join(source_path, item)}, destination = {destination_path}"
-            )
+            shutil.copy(new_item_path, destination_path)
 
 
 def main():
-    test()
+    clean_public_and_copy_static_files()
 
 
 if __name__ == "__main__":
